a8aabf7c8513585ed739d297cb37f5a7b216a543
thewhitewolf2411 <39027423+thewhitewolf2411@users.noreply.github.com>
2022-03-03 19:07:58 +0100

    adding client form

--- src/features/pages/Clients/ClientNew.jsx
+++ src/features/pages/Clients/ClientNew.jsx
@@ -1,11 +1,95 @@
 import React from "react";
 
+import Input from '../../../app/common/FormElements/Input';
+import Button from '../../../app/common/FormElements/Button';
+
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH, VALIDATOR_EMAIL} from '../../../app/util/validators'; 
+
+import { useForm } from "../../../app/util/form-hook";
+
 function ClientNew() {
 
+  const [formState, inputHandler] = useForm(
+    {
+      name: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+      email: {
+        value: "",
+        isValid: false,
+      },
+      contact_person: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      }
+    },
+    false
+  );
+
+  const clientSubmitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(formState.inputs);
+  };
+
   return (
-    <>
-      Create New Client
-    </>
+    <form className="place-form" onSubmit={clientSubmitHandler}>
+      <Input
+        id="name"
+        element="input"
+        type="text"
+        label="Client Name"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid name."
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        type="text"
+        label="Client Address"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid address."
+        onInput={inputHandler}
+      />
+      <Input
+        id="email"
+        element="input"
+        type="email"
+        label="Client Email"
+        validators={[VALIDATOR_EMAIL()]}
+        errorText="Please enter a valid email address."
+        onInput={inputHandler}
+      />
+      <Input
+        id="contact_person"
+        element="input"
+        type="text"
+        label="Contact Person"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid contact person."
+        onInput={inputHandler}
+      />
+      <Input
+        id="description"
+        label="Client Description"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid description."
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
+    </form>
   );
 }
 
--- src/features/pages/Invoices/InvoiceNew.jsx
+++ src/features/pages/Invoices/InvoiceNew.jsx
@@ -3,42 +3,19 @@ import React from "react";
 import Input from '../../../app/common/FormElements/Input';
 import Button from '../../../app/common/FormElements/Button';
 
-import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH} from '../../../app/util/validators'; 
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH, VALIDATOR_EMAIL} from '../../../app/util/validators'; 
+
+import { useForm } from "../../../app/util/form-hook";
 
 function InvoiceNew() {
 
+  const clientSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className="place-form" onSubmit={placeSubmitHandler}>
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title."
-        onInput={inputHandler}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        type="text"
-        label="Description"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description (at least 5 characters)."
-        onInput={inputHandler}
-      />
-      <Input
-        id="address"
-        element="input"
-        type="text"
-        label="Address"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid address."
-        onInput={inputHandler}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        ADD PLACE
-      </Button>
+    <form className="place-form" onSubmit={clientSubmitHandler}>
+
     </form>
   );
 }
